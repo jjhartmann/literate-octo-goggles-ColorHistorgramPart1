f47a1df458bcb5a848a03e2d708cc8140366e426
jhartman <jhartman@sfu.ca>
2016-03-23 13:07:16 -0700

    Fix: Parameters to not rely on previous position of x-y pairs.

--- ColorHistogram_Part2.m
+++ ColorHistogram_Part2.m
@@ -6,7 +6,7 @@ load('CMPT412_blackcup.mat');
 load('CMPT412_bluecup.mat');
 
 % TODO: choose what video
-video = blackcup;
+video = bluecup;
 
 % Get box input 
 frame1 = video(:,:,:,1);
@@ -21,19 +21,21 @@ model_img = imcrop(frame1, [bx by tx ty]);
 figure(2),imshow(model_img)
 
 %% Get Model Histogram
-M_histo = createcolourhistogram(model_img, 0);
+M_histo = createcolourhistogram(model_img, 100);
 
 %% Iterate through video and track object
 [h, w, d, f] = size(video);
 radius = max(ty, tx)/2 + 20;
+x = bx;
+y = by;
 for frame_index = 1:f
 
     frame = video(:,:,:,frame_index);
 
-    I_histo = createcolourhistogram(frame, 0);
+    I_histo = createcolourhistogram(frame, 100);
     R_histo = createratiohistogram(M_histo, I_histo);
     BP_img = createbackprojectionimage(R_histo, frame);
-    [x, y, pline_x, pline_y] = locateobject(BP_img, frame, radius);
+    [x, y, pline_x, pline_y] = locateobject(BP_img, frame, radius, 0, -1, -1);
 
     figure(1),imshow(frame),hold on
     plot(x, y, 'x', 'LineWidth', 3)
