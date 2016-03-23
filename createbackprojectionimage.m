a4b2462e47aa656a61321715c9438894f3a798fb
jhartman <jhartman@sfu.ca>
2016-03-23 01:08:00 -0700

    Add: max function to guarentee that the index is greater than one.

--- createbackprojectionimage.m
+++ createbackprojectionimage.m
@@ -10,9 +10,9 @@ for i = 1:h
        grn = image(i, j, 2) + 1;
        blu = image(i, j, 3) + 1;
        
-       index1 = ceil(red/16);
-       index2 = ceil(grn/16);
-       index3 = ceil(blu/16);
+       index1 = max(ceil(red/16), 1);
+       index2 = max(ceil(grn/16), 1);
+       index3 = max(ceil(blu/16), 1);
        
        R_val = R_histo(index1, index2, index3);
        BP_image(i, j) = R_val;
