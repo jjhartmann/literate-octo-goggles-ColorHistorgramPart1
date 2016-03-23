e700c558125b90bf8e1df5bd130e3258abd96ff6
jhartman <jhartman@sfu.ca>
2016-03-23 12:19:20 -0700

    Fix: change bucket size from 16 to 8

--- createratiohistogram.m
+++ createratiohistogram.m
@@ -1,12 +1,12 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%% Function to create a ratio histogram. 
 function R_histo = createratiohistogram(M_histo, I_histo)
-R_histo = double(zeros(16, 16, 16));
+R_histo = double(zeros(8, 8, 8));
 
 % Iterate over I_histo and M_histo and get the ratio min(M/I, 1);
-for i = 1:16
-    for j = 1:16
-        for k = 1:16
+for i = 1:8
+    for j = 1:8
+        for k = 1:8
             M_val = M_histo(i, j, k);
             I_val = I_histo(i, j, k);
             
