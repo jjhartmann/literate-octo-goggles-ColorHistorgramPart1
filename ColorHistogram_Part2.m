8b647ee09b8532423568d8a0c7f03355e58378fe
jhartman <jhartman@sfu.ca>
2016-03-23 00:49:46 -0700

    Add: load video objects and crop object to track from frame 1.

--- ColorHistogram_Part2.m
+++ ColorHistogram_Part2.m
@@ -1,3 +1,22 @@
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%%% Colour Histogram Part 2 - Object tracking. 
 
+
+load('CMPT412_blackcup.mat');
+load('CMPT412_bluecup.mat');
+
+% TODO: choose what video
+video = blackcup;
+
+% Get box input 
+frame1 = video(:,:,:,1);
+figure(1),imshow(frame1)
+[bx, by] = ginput(1);
+[tx, ty] = ginput(1);
+
+% Crop image
+ty = abs(by - ty);
+tx = abs(bx - tx);
+model_img = imcrop(frame1, [bx by tx ty]);
+figure(2),imshow(model_img)
+
