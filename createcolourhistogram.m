29a013e74558d425f9cde1762e0c140180d7fe64
jhartman <jhartman@sfu.ca>
2016-03-23 12:18:48 -0700

    Fix: bucket size form 16 to 8

--- createcolourhistogram.m
+++ createcolourhistogram.m
@@ -2,7 +2,7 @@
 %%% Function to create a color histogram
 function histo = createcolourhistogram(image, th) 
 model = double(image);
-histo = zeros(16, 16, 16);
+histo = zeros(8, 8, 8);
 
 %% Build the histogram for model
 [h, w, d] = size(model);
@@ -12,11 +12,12 @@ for i = 1:h
        grn = model(i, j, 2) + 1;
        blu = model(i, j, 3) + 1;
        
+       IL = red + grn + blu;
        
-       if ( red > th || grn > th || blu > th)
-           index1 = ceil(red/16);
-           index2 = ceil(grn/16);
-           index3 = ceil(blu/16);
+       if (IL > th && IL < ((3 * 256)-th))
+           index1 = ceil(red/32);
+           index2 = ceil(grn/32);
+           index3 = ceil(blu/32);
 %             index1 = ceil((abs(red - grn)+1)/16);
 %             index2 = ceil((abs((2*blu) - red - grn)+1)/32);
 %             index3 = ceil((abs(red + grn + blu)+1)/48);
