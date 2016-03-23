f64a7f347db710af372cce167694ab4cc092a306
jhartman <jhartman@sfu.ca>
2016-03-22 20:54:32 -0700

    Add: Ratio histogram
    Ration of the model/Image histograms.
    Used for backprojection.

--- ColorHistogram_Part1.m
+++ ColorHistogram_Part1.m
@@ -54,3 +54,32 @@ for i = 1:w
    end
 end
 
+
+
+%% Build Ratio histogram R
+R_histo = double(zeros(16, 16, 16));
+
+% Iterate over I_histo and M_histo and get the ratio min(M/I, 1);
+for i = 1:16
+    for j = 1:16
+        for k = 1:16
+            M_val = M_histo(i, j, k);
+            I_val = I_histo(i, j, k);
+            
+            % check for inf or nan
+            if (M_val == 0 || I_val == 0)
+                if (M_val == 0)
+                    M_val = 0;
+                    I_val = 1;
+                else
+                    M_val = 1;
+                    I_val = 1;
+                end
+            end
+            
+            R_histo(i, j, k) = min(double(M_val)/double(I_val), 1.0);
+        end
+    end
+end
+
+
